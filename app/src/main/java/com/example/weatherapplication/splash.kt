497c80b18a8431b276e41a62ac2cc30ce4ce64f8
Sufyan <contactsufyan70@gmail.com>
2024-02-11 21:20:35 -0800

    First Commit

---
+++ app/src/main/java/com/example/weatherapplication/splash.kt
@@ -0,0 +1,21 @@
+package com.example.weatherapplication
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import androidx.activity.enableEdgeToEdge
+
+class Splash : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        enableEdgeToEdge()
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+        Handler(Looper.getMainLooper()).postDelayed({
+            val intent = Intent(this,MainActivity::class.java)
+            startActivity(intent)
+            finish()
+        },3000)
+    }
+}
